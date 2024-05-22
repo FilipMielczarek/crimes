4a526f1ba6969fb4bd56a0a63b96a75f0c08a0a5
domizalewska <domzal1801@gmail.com>
2024-05-22 19:22:44 +0200

    new

--- app_crimes_comparison/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-    id = models.IntegerField()
-    email = models.CharField(max_length=150)
-    password = models.CharField(max_length=150)
